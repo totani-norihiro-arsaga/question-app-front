0457ab485adaeff45be21cfde03dcfe19eb2bbba
totani <norihiro.totani@arsaga.jp>
2024-07-13 22:03:55 +0900

    ページネーション実装

--- src/api/survey.ts
+++ src/api/survey.ts
@@ -1,13 +1,12 @@
 import axios from 'axios';
 
 const END_POINT = 'http://localhost:3000/survey';
-const PAGENATION_LIMIT_NUM = 2;
 
 const surveyApi = {
-    async index(page:number) {
-        const result = await axios.get(END_POINT + `/index?page=${page}&limit=${PAGENATION_LIMIT_NUM}`);
-        console.log(result.data);
-        return result.data.data;
+    async index() {
+        const result = await axios.get(END_POINT + `/index`);
+
+        return result.data;
     }
 }
 
--- src/pages/admin/SurveyIndex.tsx
+++ src/pages/admin/SurveyIndex.tsx
@@ -1,8 +1,9 @@
 import surveyApi from "../../api/survey";
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 import { Survey } from "../../types/survey";
+import ReactPaginate from "react-paginate";
 
 const SurveyListContainer = styled.div`
   padding: 20px;
@@ -21,32 +22,63 @@ const SurveyLink = styled(Link)`
   }
 `;
 
-const SurveyIndex: React.FC = () => {
+const SurveyIndex:React.FC = () => {
   const [surveys, setSurveys] = useState<Survey[]>([]);
+  const fetchData = async () => {
+    const data = await surveyApi.index();
+    setSurveys(data);
+  };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await surveyApi.index(2);
-      setSurveys(data);
-    };
+  const itemsPerPage = 5;
+  const [itemOffset, setItemOffset] = useState(0);
 
-    fetchData();
-  }, []);
+  const endOffset = itemOffset + itemsPerPage;
+  const currentItems = surveys.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(surveys.length / itemsPerPage);
 
-  return (
-    <SurveyListContainer>
-      <h2>Survey List</h2>
-      <ul>
-        {surveys.map(survey => (
-          <SurveyListItem key={survey.id}>
-            <SurveyLink to={`/survey/${survey.id}`}>
-              {survey.title}
-            </SurveyLink> (Created: {new Date(survey.createdDate).toLocaleString()})
-          </SurveyListItem>
-        ))}
-      </ul>
-    </SurveyListContainer>
-  );
+  const handlePageClick = (event: { selected: number }) => {
+    const newOffset = (event.selected * itemsPerPage) % surveys.length;
+    setItemOffset(newOffset);
+  }
+    useEffect(() => {
+      fetchData();
+    }, []);
+
+    return (
+      <>
+        <SurveyListContainer>
+          <h2>Survey List</h2>
+          <ul>
+            {currentItems.map((survey) => (
+              <SurveyListItem key={survey.id}>
+                <SurveyLink to={`/survey/${survey.id}`}>
+                  {survey.title}
+                </SurveyLink>{" "}
+                (Created: {new Date(survey.createdDate).toLocaleString()})
+              </SurveyListItem>
+            ))}
+          </ul>
+          <ReactPaginate
+            previousLabel="Previous"
+            nextLabel="Next"
+            pageClassName="page-item"
+            pageLinkClassName="page-link"
+            previousClassName="page-item"
+            previousLinkClassName="page-link"
+            nextClassName="page-item"
+            nextLinkClassName="page-link"
+            breakLabel="..."
+            breakClassName="page-item"
+            breakLinkClassName="page-link"
+            pageCount={pageCount}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={handlePageClick}
+            containerClassName="pagination"
+            activeClassName="active"
+          />
+        </SurveyListContainer>
+      </>
+    );
 };
-
 export default SurveyIndex;
